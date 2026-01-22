1bcaf34d8538bc10ffa0e3c46aec13aad9cd76f9
drexa1 <drexa1@hotmail.com>
2026-01-22 19:30:06 +0100

    Dev binaries

--- miner/src/py/main.py
+++ miner/src/py/main.py
@@ -1,19 +1,21 @@
+import copy
 import sys
-from lib.src.types.py.block import Block
+from pathlib import Path
+from lib.src.types.py.block import Block, BlockHeader
 
 
 def main():
     # Parse block path and steps count arguments
     if len(sys.argv) != 3:
-        path = sys.argv[1]
-        steps = int(sys.argv[2])
-    else:
         print("Usage: miner <block_file> <steps>", file=sys.stderr)
         sys.exit(1)
-    og_block = Block.load_from_file(path)
-    block = og_block.clone()
+    else:
+        path = Path(sys.argv[1])
+        steps = int(sys.argv[2])
+    og_block: Block = Block.load(path)
+    block: Block = copy.deepcopy(og_block)
     while not block.header.mine(steps):
-        print("mining...")
+        print(f"mining... nonce={block.header.nonce}")
     # Print original block and its hash
     print("original:", repr(og_block))
     print("hash:", og_block.header.hash())
